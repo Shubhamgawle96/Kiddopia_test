9b898587e3abd426aa4775b2fa457af0a5a14a33
Shubham Gawle <shubham.gawle@ril.com>
2021-04-04 21:41:21 +0530

    final

--- app/routes.py
+++ app/routes.py
@@ -1,105 +1,112 @@
 import os
 from uuid import uuid4
 from app import app
+import itertools
+import time
+import json
+
 from firebase import firebase
-from flask import Flask, render_template, request,send_file,jsonify,flash,url_for,redirect
+from flask import render_template, request,send_file
 from werkzeug.utils import secure_filename
 from app.forms import Form
 
-firebase = firebase.FirebaseApplication('https://kiddopia-73f9e-default-rtdb.firebaseio.com/', None)
-
+firebase = firebase.FirebaseApplication(app.config['DB_URI'], None)
 
 @app.route('/')
 @app.route('/home')
 def home():
-  result = firebase.get('/Users', None)
-  print("result",type(result),result)
-  return render_template("home.html", places=result,form=Form(request.form))
+    try:
+        result = firebase.get('/Users', None)
+        return render_template("home.html", places=result,form=Form(request.form))
+    except Exception as e:
+        raise e
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
-    form = Form(request.form)
-    if request.method == 'POST' and form.validate():
+    try:
+        form = Form(request.form)
+        if request.method == 'POST' and form.validate():
 
-      # will register fields called 'username' and 'email'.
+          # will register fields called 'username' and 'email'.
 
-        userdata = dict(request.form)
-        print("userdata",type(userdata),userdata)
-        name = userdata["name"]
-        email = userdata["email"]
-        f = request.files['file']
+            userdata = dict(request.form)
+            name = userdata["name"]
+            email = userdata["email"]
+            f = request.files['file']
 
-        primary_key = str(uuid4())
-        uploads_dir = os.path.join(os.path.dirname(__file__),'uploads_dir')
-        f.save(os.path.join(uploads_dir, secure_filename(primary_key)))
-        new_data = {"name": name, "email": email,"attachment":f.filename,"primary_key":primary_key}
-        print("new_data_submit",new_data)
-        firebase.post('/Users',data=new_data)
-        result = firebase.get('/Users', None)
-        return render_template("home.html", places=result,form=Form(request.form))
-    else:
-      result = firebase.get('/Users', None)
-      return render_template("home.html",places = result,error = form.errors,form=Form(request.form))
+            primary_key = str(uuid4())
+            uploads_dir = os.path.join(os.path.dirname(__file__),'uploads_dir')
+            f.save(os.path.join(uploads_dir, secure_filename(primary_key)))
+            new_data = {"name": name, "email": email,"attachment":f.filename,"primary_key":primary_key}
+            firebase.post('/Users',data=new_data)
+            result = firebase.get('/Users', None)
+            return render_template("home.html", places=result,form=Form(request.form))
+        else:
+          result = firebase.get('/Users', None)
+          return render_template("home.html",places = result,error = form.errors,form=Form(request.form))
+    except Exception as e:
+        raise e
 
 
 
 
 @app.route('/edit', methods=['GET', 'POST'])
 def edit():
-    form = Form(request.form)
-    if request.method == 'POST' and form.validate():
-        userdata = dict(request.form)
-        print("edit data", type(userdata), userdata)
-        id = userdata["id"]
-        name = userdata["name"]
-        email = userdata["email"]
-        f = request.files['file']
+    try:
+        form = Form(request.form)
+        if request.method == 'POST' and form.validate():
+            userdata = dict(request.form)
+            id = userdata["id"]
+            name = userdata["name"]
+            email = userdata["email"]
+            f = request.files['file']
 
-        if f.filename == '':
-          edit_url = "/Users/" + str(id)
-          firebase.put(url=edit_url, name='name', data=name)
-          firebase.put(url=edit_url, name='email', data=email)
+            if f.filename == '':
+              edit_url = "/Users/" + str(id)
+              firebase.put(url=edit_url, name='name', data=name)
+              firebase.put(url=edit_url, name='email', data=email)
+            else:
+              primary_key = str(uuid4())
+              uploads_dir = os.path.join(os.path.dirname(__file__), 'uploads_dir')
+              f.save(os.path.join(uploads_dir, secure_filename(primary_key)))
+              new_data = {"name": name, "email": email, "attachment": f.filename, "primary_key": primary_key}
+              firebase.put(url='/Users',name=id,data=new_data)
+
+
+            result = firebase.get('/Users', None)
+            return render_template("home.html", places=result,form=Form(request.form))
         else:
-          primary_key = str(uuid4())
-          uploads_dir = os.path.join(os.path.dirname(__file__), 'uploads_dir')
-          f.save(os.path.join(uploads_dir, secure_filename(primary_key)))
-          new_data = {"name": name, "email": email, "attachment": f.filename, "primary_key": primary_key}
-          print("new_edit_data",new_data)
-          firebase.put(url='/Users',name=id,data=new_data)
-
-
-        # print("post_result", type(result), result)
-        result = firebase.get('/Users', None)
-        return render_template("home.html", places=result,form=Form(request.form))
-    else:
-        result = firebase.get('/Users', None)
-        return render_template("home.html", places=result, error=form.errors, form=Form(request.form))
+            result = firebase.get('/Users', None)
+            return render_template("home.html", places=result, error=form.errors, form=Form(request.form))
+    except Exception as e:
+        raise e
 
 @app.route('/delete', methods=['GET', 'POST'])
 def delete():
-  if request.method == 'POST' and len(dict(request.form)) > 0:
-    userdata = dict(request.form)
-    print("delete data", type(userdata), userdata)
-    id = userdata["id"]
-    firebase.delete(url="/Users",name=id)
-    result = firebase.get('/Users', None)
-    return render_template("home.html", places=result)
-  else:
-    return "Sorry, there was an error."
+    try:
+        if request.method == 'POST' and len(dict(request.form)) > 0:
+            userdata = dict(request.form)
+            id = userdata["id"]
+            firebase.delete(url="/Users",name=id)
+            result = firebase.get('/Users', None)
+            return render_template("home.html", places=result)
+        else:
+            return "Sorry, there was an error."
+    except Exception as e:
+        raise e
 
 @app.route('/downloads/<path:id>', methods=['GET', 'POST'])
 def downloads(id):
-    print("helllo shubham")
-    print(" download id",id)
-    user = firebase.get('/Users',name=id)
-    attachment_name = user['attachment']
-    filename = user['primary_key']
-    print("attachment",attachment_name)
+    try:
+        user = firebase.get('/Users',name=id)
+        attachment_name = user['attachment']
+        filename = user['primary_key']
 
-    file_path = os.path.join(os.path.dirname(__file__), 'uploads_dir',filename)
-    print("file_path",type(file_path),file_path)
+        file_path = os.path.join(os.path.dirname(__file__), 'uploads_dir',filename)
 
-    return send_file(file_path, as_attachment=True,attachment_filename=attachment_name)
+        return send_file(file_path, as_attachment=True,attachment_filename=attachment_name)
+    except Exception as e:
+        raise e
 
 
 
